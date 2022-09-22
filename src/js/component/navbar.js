fc88da5b3eec93d129a4f7a573fdee8715fdd455
Noé G <noogux@gmail.com>
2022-09-22 00:37:49 -0400

    Update navbar.js

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,20 +5,18 @@ export const Navbar = () => {
   return (
     <nav className="navbar navbar-light bg-light mb-3">
       <Link to="/">
-        <span className="navbar-brand mb-0 h1">
-          <a class="navbar-brand mx-5" href="#">
-            <img src="https://www.kindpng.com/picc/m/27-270569_transparent-star-wars-logo-png-star-wars-png.png" 
-  alt="Bootstrap"
-  width={50}
-  height={35}
-/>
-
-          </a>
+        <span className="navbar-brand mx-4 h1">
+          <img
+            src="https://www.kindpng.com/picc/m/27-270569_transparent-star-wars-logo-png-star-wars-png.png"
+            alt="Bootstrap"
+            width={50}
+            height={34}
+          />
         </span>
       </Link>
       <div className="ml-auto">
         <Link to="/demo">
-          <button className="btn btn-primary mx-5 px-4 py-2">
+          <button className="btn btn-primary mx-5">
             Favorites
           </button>
         </Link>
