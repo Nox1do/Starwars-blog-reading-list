0565da5e96c0ea5676f7a987e9c1f231685e620d
Nox1do <noogux@gmail.com>
2022-09-26 06:46:06 +0000

    adding fetch

--- src/js/views/card.js
+++ src/js/views/card.js
@@ -3,19 +3,19 @@ import {Link} from 'react-router-dom';
 import { BsHeart } from 'react-icons/bs';
 
 
-   export const Card = (type) => {
+   export const Card = (props) => {
 return (
 	<div className="card mx-3" style={{ width: "18rem" }}>
   <img src="https://fakeimg.pl/400x200/" className="card-img-top" alt="..." />
   <div className="card-body">
-    <h5 className="card-title">Luke Skywalker</h5>
+    <h5 className="card-title">{props.name}</h5>
     <p className="card-text">
     Genre: Male <br/>
 	  Hair Color: blond <br/>
 	  Eye Color: blue <br/>
     </p>
 	<div className="d-flex justify-content-between pb-2 ">
-    <Link to={() => type === '/Planets' ? '/Characters': '/Planets' } className="btn btn-outline-primary">
+    <Link to={() => props === '/Characters' ? '/Characters': '/Planets' } className="btn btn-outline-primary">
       Learn More!
     </Link>
     <Link to='/Planets' className="btn btn-outline-warning">
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,33 +1,55 @@
-import React from "react";
+import React, { useEffect, useState, useSyncExternalStore } from "react";
 import "../../styles/home.css";
 import Card from "./card";
-import Characters from "./Characters";
 
-export const Home = () => (
-  <>
-   <h1 className="mx-3" style={{ color: "red" }}>Characters</h1>
+
+
+
+
+
+export const Home = () => {
+//useState
+const [players, setPlayers] = useState([]);
+const [planets, setPlanets] = useState([]);
+
+//fetch
+const getCharacters = () => {
+   fetch("https://www.swapi.tech/api/people/")
+  .then((resp) => resp.json())
+  .then((datos) => setPlayers(datos.results))
+  .catch((err) => console.error(err));
+};
+console.log(players.name)
+
+useEffect(()=> {
+  getCharacters();
+
+},[]);
+
+return (
+  <> 
+  <h1 className="mx-3" style={{ color: "red" }}>Characters</h1>
   <div className=" row flex-row flex-nowrap overflow-scroll mx-2 py-2">    
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-  </div>
-
+  {players.map((item, index) => {
+              return (
+                <Card key={index} name={item.name} type="players" id={item.uid}/>
+              );
+            })}
+		</div>
+	
+             
+  
     <h1 className="mx-3" style={{ color: "red" }}>Planets</h1>
   <div className=" row flex-row flex-nowrap overflow-scroll mx-2">
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-  </div>
+  {players.map((item, i) => {
+              return (
+                <Card key={i} name={item.name} type="planets" id={item.uid}/>
+              );
+            })}
+		</div>
+   
+  
     
     </>
 );
-
-
-
-
+};
