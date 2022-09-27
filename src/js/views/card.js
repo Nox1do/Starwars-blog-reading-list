1314830b4a59baa455395febbbcce6fcbec2419e
Nox1do <noogux@gmail.com>
2022-09-27 20:51:56 +0000

    fin

--- src/js/views/card.js
+++ src/js/views/card.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
 import { BsHeart } from "react-icons/bs";
-
+import { Context } from "../store/appContext";
+import { useContext, useEffect } from "react";
 
 export const Card = (props) => {
-  
+  const { store, actions } = useContext(Context);
+
   return (
     <div className="card mx-3" style={{ width: "18rem" }}>
       <img
@@ -16,10 +18,8 @@ export const Card = (props) => {
       />
       <div className="card-body">
         <h5 className="card-title">{props.name}</h5>
-        <p className="card-text">
-          <b>Name:</b> 
-          <hr />
-        </p>
+        <p className="card-text"></p>
+        <hr />
         <div className="d-flex justify-content-between pb-2 ">
           <Link
             to={() =>
